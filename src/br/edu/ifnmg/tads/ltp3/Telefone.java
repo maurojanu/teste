90eb037893800b3fac52aa6a0d3bdb901142e626
aluno <aluno@aluno-desktop>
2013-03-21 09:11:45 -0300

    commit inicial 1

---
+++ src/br/edu/ifnmg/tads/ltp3/Telefone.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.ltp3;
+
+/**
+ *
+ * @author aluno
+ */
+public class Telefone {
+    
+    private int id;
+    private int ddd;
+    private int num;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDdd() {
+        return ddd;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + this.id;
+        hash = 89 * hash + this.ddd;
+        hash = 89 * hash + this.num;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Telefone other = (Telefone) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.ddd != other.ddd) {
+            return false;
+        }
+        if (this.num != other.num) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefone{" + "id=" + id + ", ddd=" + ddd + ", num=" + num + '}';
+    }
+    
+    
+}
